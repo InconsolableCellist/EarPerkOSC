3fe7d9db677233160de941f6481abde3f8296e09
user <user@user.com>
2024-02-06 14:08:13 -0700

    Breaking functionality out into functions, and adding overwhelming loud option

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 use std::io::{Write};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 extern crate rosc;
 use rosc::{OscPacket, OscType, OscMessage};
@@ -47,13 +47,15 @@ fn create_config_ini_if_not_exists() -> Result<(), std::io::Error> {
         .set("address", "127.0.0.1")
         .set("port", "9000")
         .set("osc_address_left", "/avatar/parameters/EarPerkLeft")
-        .set("osc_address_right", "/avatar/parameters/EarPerkRight");
+        .set("osc_address_right", "/avatar/parameters/EarPerkRight")
+        .set("osc_address_overwhelmingly_loud", "/avatar/parameters/EarOverwhelm");
     config.with_section(Some("audio"))
         .set("input_device", "CABLE Output (VB-Audio Virtual Cable)")
         .set("differential_threshold", "0.01")
-        .set("reset_timeout_ms", "1000")
-        .set("timeout_ms", "100")
-        .set("buffer_size_ms", "100");
+        .set("volume_threshold", "0.1")
+        .set("excessive_volume_threshold", "0.4")
+        .set("reset_timeout_ms", "500")
+        .set("timeout_ms", "100");
     config.write_to_file("config.ini").unwrap();
     Ok(())
 }
@@ -87,18 +89,23 @@ fn main() {
 
     let mut last_left_message_timestamp = std::time::Instant::now();
     let mut last_right_message_timestamp = std::time::Instant::now();
+    let mut last_overwhelm_timestamp = std::time::Instant::now();
 
     let mut left_perked = false;
     let mut right_perked = false;
+    let mut overwhelmingly_loud = false;
 
     let config = read_config_ini().unwrap();
     let address_left = config.section(Some("connection")).unwrap().get("osc_address_left").unwrap().to_owned();
     let address_right = config.section(Some("connection")).unwrap().get("osc_address_right").unwrap().to_owned();
+    let address_overwhelmingly_loud = config.section(Some("connection")).unwrap().get("osc_address_overwhelmingly_loud").unwrap().to_owned();
     let target_address = format!("{}:{}", config.section(Some("connection")).unwrap().get("address").unwrap(), config.section(Some("connection")).unwrap().get("port").unwrap());
-    let threshold = config.section(Some("audio")).unwrap().get("differential_threshold").unwrap().parse::<f32>().unwrap();
+    let differential_threshold = config.section(Some("audio")).unwrap().get("differential_threshold").unwrap().parse::<f32>().unwrap();
+    let volume_threshold = config.section(Some("audio")).unwrap().get("volume_threshold").unwrap().parse::<f32>().unwrap();
+    let excessive_volume_threshold = config.section(Some("audio")).unwrap().get("excessive_volume_threshold").unwrap().parse::<f32>().unwrap();
+
     let reset_timeout = Duration::from_millis(config.section(Some("audio")).unwrap().get("reset_timeout_ms").unwrap().parse::<u64>().unwrap());
     let timeout = Duration::from_millis(config.section(Some("audio")).unwrap().get("timeout_ms").unwrap().parse::<u64>().unwrap());
-    let buffer_size_ms = config.section(Some("audio")).unwrap().get("buffer_size_ms").unwrap().parse::<u64>().unwrap();
     let running = Arc::new(AtomicBool::new(true));
     let r = running.clone();
 
@@ -109,7 +116,8 @@ fn main() {
     print_banner();
 
     println!("Now listening for stereo audio and sending OSC messages for ear perk on/off...");
-    println!("L: perk left ear, R: perk right ear, B: perk both ears, !L: reset left ear, !R: reset right ear\n\n");
+    println!("L: perk left ear, R: perk right ear, B: perk both ears, !L: reset left ear, !R: reset right ear\n\
+        O: Overwhelmingly loud!, !O: Overwhelming reset\n");
 
     unsafe {
         CoInitializeEx(null_mut(), COINITBASE_MULTITHREADED);
@@ -165,6 +173,12 @@ fn main() {
             &mut capture_client as *mut _ as *mut _,
         );
 
+        if capture_client.is_null() {
+            println!("Failed to get capture client");
+            println!("This might happen because the audio device is in use by another application. Please close any other applications using the audio device and try again.");
+            return;
+        }
+
         // Start the audio stream
         (*audio_client).Start();
 
@@ -195,49 +209,17 @@ fn main() {
                 (left_avg, right_avg) = calculate_avg_lr_new(&samples);
                 let current_time = std::time::Instant::now();
 
-                // if the left is louder than the right by a threshold, perk the left ear
-                // if the right is louder than the left by a threshold, perk the right ear
-                // if the left and right are within the threshold of each other, perk both
-                // if the left and right are below the threshold, reset both
-                if left_avg - right_avg > threshold {
-                    if current_time - last_left_message_timestamp > timeout {
-                        print!("L");
-                        send_osc_message(&address_left, &args_true, target_address.clone());
-                        last_left_message_timestamp = current_time;
-                        left_perked = true;
-                    }
-                } else if right_avg - left_avg > threshold {
-                    if current_time - last_right_message_timestamp > timeout {
-                        print!("R");
-                        send_osc_message(&address_right, &args_true, target_address.clone());
-                        last_right_message_timestamp = current_time;
-                        right_perked = true;
-                    }
-                } else if left_avg > threshold && right_avg > threshold {
-                    if current_time - last_left_message_timestamp > timeout &&
-                        current_time - last_right_message_timestamp > timeout {
-                        print!("B");
-                        send_osc_message(&address_left, &args_true, target_address.clone());
-                        send_osc_message(&address_right, &args_true, target_address.clone());
-                        last_left_message_timestamp = current_time;
-                        last_right_message_timestamp = current_time;
-                        left_perked = true;
-                        right_perked = true;
-                    }
-                } else {
-                    if left_perked && current_time - last_left_message_timestamp > reset_timeout {
-                        print!("!L\n");
-                        send_osc_message(&address_left, &args_false, target_address.clone());
-                        left_perked = false;
-                    }
-                    if right_perked && current_time - last_right_message_timestamp > reset_timeout {
-                        print!("!R\n");
-                        send_osc_message(&address_right, &args_false, target_address.clone());
-                        right_perked = false;
-                    }
+                // Fold both ears if the volume is overwhelmingly loud (exceeding `excessive_volume_threshold`). Also
+                // reset both ears if it's not.
+                // If it's not, proceed to perk left, right, both, or unperk based on volume differential and volume, and timing
+                process_vol_overwhelm(&args_true, &args_false, &address_overwhelmingly_loud, &target_address, excessive_volume_threshold, left_avg,
+                                                            right_avg, &mut last_overwhelm_timestamp, current_time, reset_timeout, &mut overwhelmingly_loud);
+                if !overwhelmingly_loud {
+                    process_vol_perk_and_reset(&args_true, &args_false, &mut last_left_message_timestamp, &mut last_right_message_timestamp,
+                                               &mut left_perked, &mut right_perked, &address_left, &address_right, &target_address,
+                                               differential_threshold, volume_threshold, reset_timeout, timeout, left_avg, right_avg, current_time);
                 }
                 io::stdout().flush().unwrap();
-
 
                 (*capture_client).ReleaseBuffer(frames_available);
                 (*capture_client).GetNextPacketSize(&mut packet_length);
@@ -260,6 +242,109 @@ fn main() {
 
         CoUninitialize();
 
+    }
+}
+
+/// This function processes the volume and checks for ear perking conditions.
+/// If the left channel's average volume is louder than the right by a certain threshold, it sends an OSC message to perk the left ear.
+/// If the right channel's average volume is louder than the left by a certain threshold, it sends an OSC message to perk the right ear.
+/// If the average volumes of both channels are within the threshold of each other and above a volume threshold, it sends OSC messages to perk both ears.
+/// If the average volumes of both channels are below the volume threshold, it sends OSC messages to reset both ears.
+///
+/// # Arguments
+/// * `args_true` - The arguments to be sent with the OSC message when the ear is to be perked.
+/// * `args_false` - The arguments to be sent with the OSC message when the ear is to be reset.
+/// * `last_left_message_timestamp` - The timestamp of the last message sent to the left ear.
+/// * `last_right_message_timestamp` - The timestamp of the last message sent to the right ear.
+/// * `left_perked` - A boolean indicating whether the left ear is perked.
+/// * `right_perked` - A boolean indicating whether the right ear is perked.
+/// * `address_left` - The OSC address for the left ear.
+/// * `address_right` - The OSC address for the right ear.
+/// * `target_address` - The target address to send the OSC message to.
+/// * `differential_threshold` - The minimum difference between the left and right channels to trigger a left or right-only ear perk.
+/// * `volume_threshold` - The minimum volume to trigger an ear perk.
+/// * `reset_timeout` - The time to wait after the last sound before sending the unperk message.
+/// * `timeout` - The time to wait before trying to perk the ear again.
+/// * `left_avg` - The average volume of the left channel.
+/// * `right_avg` - The average volume of the right channel.
+/// * `current_time` - The current time.
+fn process_vol_perk_and_reset(args_true: &Vec<OscType>, args_false: &Vec<OscType>, last_left_message_timestamp: &mut Instant,
+                              last_right_message_timestamp: &mut Instant, left_perked: &mut bool, right_perked: &mut bool,
+                              address_left: &String, address_right: &String, target_address: &String, differential_threshold: f32,
+                              volume_threshold: f32, reset_timeout: Duration, timeout: Duration, left_avg: f32, right_avg: f32, current_time: Instant) {
+    if (left_avg - right_avg > differential_threshold) && left_avg > volume_threshold {
+        if current_time - *last_left_message_timestamp > timeout {
+            print!("L");
+            send_osc_message(&address_left, &args_true, target_address.clone());
+            *last_left_message_timestamp = current_time;
+            *left_perked = true;
+        }
+    } else if (right_avg - left_avg > differential_threshold) && right_avg > volume_threshold {
+        if current_time - *last_right_message_timestamp > timeout {
+            print!("R");
+            send_osc_message(&address_right, &args_true, target_address.clone());
+            *last_right_message_timestamp = current_time;
+            *right_perked = true;
+        }
+    } else if left_avg > differential_threshold
+        && right_avg > differential_threshold
+        && left_avg > volume_threshold
+        && right_avg > volume_threshold {
+        if current_time - *last_left_message_timestamp > timeout &&
+            current_time - *last_right_message_timestamp > timeout {
+            print!("B");
+            send_osc_message(&address_left, &args_true, target_address.clone());
+            send_osc_message(&address_right, &args_true, target_address.clone());
+            *last_left_message_timestamp = current_time;
+            *last_right_message_timestamp = current_time;
+            *left_perked = true;
+            *right_perked = true;
+        }
+    } else {
+        if *left_perked && current_time - *last_left_message_timestamp > reset_timeout {
+            print!("!L\n");
+            send_osc_message(&address_left, &args_false, target_address.clone());
+            *left_perked = false;
+        }
+        if *right_perked && current_time - *last_right_message_timestamp > reset_timeout {
+            print!("!R\n");
+            send_osc_message(&address_right, &args_false, target_address.clone());
+            *right_perked = false;
+        }
+    }
+}
+
+/// This function processes the volume and checks if it is overwhelmingly loud.
+/// If the average volume of either the left or right channel exceeds the `excessive_volume_threshold`,
+/// it sends an OSC message with `args_true` to the `address_overwhelmingly_loud` and returns `true`.
+/// If the volume does not exceed the threshold, it sends an OSC message with `args_false` and returns `false`.
+/// No action is taken if the volume is not overwhelmingly loud and the last message was sent less than `reset_timeout` ago.
+///
+/// # Arguments
+/// * `args_true` - The arguments to be sent with the OSC message when the volume is overwhelmingly loud.
+/// * `args_false` - The arguments to be sent with the OSC message when the volume is not overwhelmingly loud.
+/// * `address_overwhelmingly_loud` - The OSC address to send the message to.
+/// * `target_address` - The target address to send the OSC message to.
+/// * `excessive_volume_threshold` - The threshold for the volume to be considered overwhelmingly loud.
+/// * `left_avg` - The average volume of the left channel.
+/// * `right_avg` - The average volume of the right channel.
+/// * `last_overwhelm_timestamp` - The timestamp of the last message sent for an overwhelmingly loud volume.
+/// * `current_time` - The current time.
+/// * `reset_timeout` - The time to wait before sending the reset message.
+/// * `overwhelmingly_loud` - A boolean indicating the current state of whether the volume is overwhelmingly loud.
+fn process_vol_overwhelm(args_true: &Vec<OscType>, args_false: &Vec<OscType>, address_overwhelmingly_loud: &String,
+                         target_address: &String, excessive_volume_threshold: f32, left_avg: f32, right_avg: f32,
+                         last_overwhelm_timestamp: &mut Instant, current_time: Instant, reset_timeout: Duration,
+                         overwhelmingly_loud: &mut bool) {
+    if left_avg > excessive_volume_threshold || right_avg > excessive_volume_threshold {
+        print!("O");
+        send_osc_message(&address_overwhelmingly_loud, &args_true, target_address.clone());
+        *last_overwhelm_timestamp = current_time;
+        *overwhelmingly_loud = true;
+    } else if *overwhelmingly_loud && current_time - *last_overwhelm_timestamp > reset_timeout {
+        print!("!O");
+        send_osc_message(&address_overwhelmingly_loud, &args_false, target_address.clone());
+        *overwhelmingly_loud = false;
     }
 }
 
